ee873a3642be7ec11b17f0383b94bfa98b8f6808
Pavel Roskin <plroskin@gmail.com>
2019-02-24 22:16:27 -0800

    Provide environment variables to downstream builds
    The cause would only be StashCause for the the top-level build of a
    multi-configuration project.
    
    First make sure it's an AbstractBuild. Non-build runs (e.g. git
    invocations) should not need our environment variables (the variables are
    substituted explicitly before the command is run).
    
    Then find the root build and check it's cause.
    
    Don't return prematurely to ensure that super.buildEnvironmentFor() is
    run unconditionally.

--- src/main/java/stashpullrequestbuilder/stashpullrequestbuilder/StashAditionalParameterEnvironmentContributor.java
+++ src/main/java/stashpullrequestbuilder/stashpullrequestbuilder/StashAditionalParameterEnvironmentContributor.java
@@ -12,21 +12,24 @@ import java.util.*;
 public class StashAditionalParameterEnvironmentContributor extends EnvironmentContributor {
     @Override
     public void buildEnvironmentFor(@Nonnull Run r, @Nonnull EnvVars envs, @Nonnull TaskListener listener) throws IOException, InterruptedException {
-        StashCause cause = (StashCause) r.getCause(StashCause.class);
-        if (cause == null) {
-            return;
-        }
+        if (r instanceof AbstractBuild) {
+            AbstractBuild build = (AbstractBuild) r;
+            AbstractBuild rootBuild = build.getRootBuild();
 
-        putEnvVar(envs, "sourceBranch", cause.getSourceBranch());
-        putEnvVar(envs, "targetBranch", cause.getTargetBranch());
-        putEnvVar(envs, "sourceRepositoryOwner", cause.getSourceRepositoryOwner());
-        putEnvVar(envs, "sourceRepositoryName", cause.getSourceRepositoryName());
-        putEnvVar(envs, "pullRequestId", cause.getPullRequestId());
-        putEnvVar(envs, "destinationRepositoryOwner", cause.getDestinationRepositoryOwner());
-        putEnvVar(envs, "destinationRepositoryName", cause.getDestinationRepositoryName());
-        putEnvVar(envs, "pullRequestTitle", cause.getPullRequestTitle());
-        putEnvVar(envs, "sourceCommitHash", cause.getSourceCommitHash());
-        putEnvVar(envs, "destinationCommitHash", cause.getDestinationCommitHash());
+            StashCause cause = (StashCause) rootBuild.getCause(StashCause.class);
+            if (cause != null) {
+                putEnvVar(envs, "sourceBranch", cause.getSourceBranch());
+                putEnvVar(envs, "targetBranch", cause.getTargetBranch());
+                putEnvVar(envs, "sourceRepositoryOwner", cause.getSourceRepositoryOwner());
+                putEnvVar(envs, "sourceRepositoryName", cause.getSourceRepositoryName());
+                putEnvVar(envs, "pullRequestId", cause.getPullRequestId());
+                putEnvVar(envs, "destinationRepositoryOwner", cause.getDestinationRepositoryOwner());
+                putEnvVar(envs, "destinationRepositoryName", cause.getDestinationRepositoryName());
+                putEnvVar(envs, "pullRequestTitle", cause.getPullRequestTitle());
+                putEnvVar(envs, "sourceCommitHash", cause.getSourceCommitHash());
+                putEnvVar(envs, "destinationCommitHash", cause.getDestinationCommitHash());
+            }
+        }
 
         super.buildEnvironmentFor(r, envs, listener);
     }
