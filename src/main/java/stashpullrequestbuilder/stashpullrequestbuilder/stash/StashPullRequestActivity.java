7b8366a43b7c0297c7948cd93cf7d5aed8991937
Pavel Roskin <plroskin@gmail.com>
2019-07-12 20:29:55 -0700

    StashPullRequestActivity: Don't implement Comparable, it is not used
    Remove FindBugs suppressions.

--- src/main/java/stashpullrequestbuilder/stashpullrequestbuilder/stash/StashPullRequestActivity.java
+++ src/main/java/stashpullrequestbuilder/stashpullrequestbuilder/stash/StashPullRequestActivity.java
@@ -1,12 +1,10 @@
 package stashpullrequestbuilder.stashpullrequestbuilder.stash;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 /** Created by Nathan on 20/03/2015. */
-@SuppressFBWarnings("EQ_COMPARETO_USE_OBJECT_EQUALS")
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class StashPullRequestActivity implements Comparable<StashPullRequestActivity> {
+public class StashPullRequestActivity {
   private StashPullRequestComment comment;
 
   public StashPullRequestComment getComment() {
@@ -15,22 +13,5 @@ public class StashPullRequestActivity implements Comparable<StashPullRequestActi
 
   public void setComment(StashPullRequestComment comment) {
     this.comment = comment;
-  }
-
-  @Override
-  public int compareTo(StashPullRequestActivity target) {
-    if (this.comment == null || target.getComment() == null) {
-      return -1;
-    }
-    int commentIdThis = this.comment.getCommentId();
-    int commentIdOther = target.getComment().getCommentId();
-
-    if (commentIdThis > commentIdOther) {
-      return 1;
-    } else if (commentIdThis == commentIdOther) {
-      return 0;
-    } else {
-      return -1;
-    }
   }
 }
